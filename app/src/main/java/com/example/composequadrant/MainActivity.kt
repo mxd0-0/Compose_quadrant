1832b7fd16be90b4695de812e87b7b10db592aec
Mohammed <mo7amxd.7@gmail.com>
2024-06-07 13:10:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.composequadrant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+
+        }
+    }
+}
+
+@Composable
+fun CompasbleQuadrant() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+
+        Row(modifier = Modifier.weight(1f)) {
+            InfoCard(
+                color = Color(0xFFEADDFF),
+                title = stringResource(R.string.first_title),
+                description = stringResource(R.string.first_description),
+                modifier = Modifier.weight(1f)
+            )
+            InfoCard(
+                color = Color(0xFFD0BCFF),
+                title = stringResource(R.string.second_title),
+                description = stringResource(R.string.second_description),
+                modifier = Modifier.weight(1f)
+            )
+
+        }
+        Row(modifier = Modifier.weight(1f)) {
+            InfoCard(
+                color = Color(0xFFB69DF8),
+                title = stringResource(R.string.third_title),
+                description = stringResource(R.string.third_description),
+                modifier = Modifier.weight(1f)
+            )
+            InfoCard(
+                color = Color(0xFFF6EDFF),
+                title = stringResource(R.string.fourth_title),
+                description = stringResource(R.string.fourth_description),
+                modifier = Modifier.weight(1f)
+            )
+
+
+        }
+    }
+
+}
+
+@Composable
+fun InfoCard(
+    title: String, description: String, color: Color, modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = title,
+            modifier = modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = description, textAlign = TextAlign.Justify)
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun QuadrantPreview() {
+    CompasbleQuadrant()
+
+
+
+
+
+}
+
